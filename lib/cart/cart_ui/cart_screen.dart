53402fd16ec39493579b10e4fe50623d6a0ef0ef
amrutapatil <amrutapatil@qnap.com>
2024-01-18 08:47:48 +0530

    Removed commented codes

--- lib/cart/cart_ui/cart_screen.dart
+++ lib/cart/cart_ui/cart_screen.dart
@@ -1,6 +1,6 @@
 import 'package:bloc_cart/cart/cartBloc_folder/cart_bloc.dart';
 import 'package:bloc_cart/cart/cart_ui/cart_tile_widget.dart';
-import 'package:bloc_cart/home/home_ui/product_tile_widget.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -57,53 +57,3 @@ class _CartScreenState extends State<CartScreen> {
     );
   }
 }
-
-
-
-
-// class Cart extends StatefulWidget {
-//   const Cart({super.key});
-
-//   @override
-//   State<Cart> createState() => _CartState();
-// }
-
-// class _CartState extends State<Cart> {
-//   final CartBloc cartBloc = CartBloc();
-//   @override
-//   void initState() {
-//     cartBloc.add(CartInitialEvent());
-//     super.initState();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Cart Items'),
-//       ),
-//       body: BlocConsumer<CartBloc, CartState>(
-//         bloc: cartBloc,
-//         listener: (context, state) {},
-//         listenWhen: (previous, current) => current is CartActionState,
-//         buildWhen: (previous, current) => current is! CartActionState,
-//         builder: (context, state) {
-//           switch (state.runtimeType) {
-//             case CartSuccessState:
-//               final successState = state as CartSuccessState;
-//               return ListView.builder(
-//                   itemCount: successState.cartItems.length,
-//                   itemBuilder: (context, index) {
-//                     return CartTileWidget(
-//                         cartBloc: cartBloc,
-//                         productDataModel: successState.cartItems[index]);
-//                   });
-
-//             default:
-//           }
-//           return Container();
-//         },
-//       ),
-//     );
-//   }
-// }
